db2664c4090e009236001fd2e21c12e1fe722fc8
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-18 15:51:13 +0700

    Model

---
+++ app/Roles.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Roles extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'roles';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'code';
+    public $incrementing = false;
+    // protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'position'
+    ];
+}
--- app/User.php
+++ app/User.php
@@ -11,14 +11,46 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $dateFormat = 'Y-m-d H:i:s.u';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        // 'name', 'email', 'password',
+        'user_id',
+        'user_name',
+        'password',
+        'user_image',
+        'day_of_birth',
+        'user_email',
+        'user_phone',
+        'user_address',
+        'user_role',
+        'user_state',
     ];
 
+    public function role()
+    {
+        return $this->belongsTo('App\Roles', 'user_role');
+    }
     /**
      * The attributes that should be hidden for arrays.
      *
