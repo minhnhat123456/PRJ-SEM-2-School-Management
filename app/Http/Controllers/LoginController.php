db53b217f2d2053ecd50f24d1808fdcddc0488f4
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-18 15:53:40 +0700

    Login service

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user_name = $request->input('user-name');
+        $password = $request->input('password');
+
+        $user = User::where('user_id', $user_name)->first();
+
+        if ($user->password == $password) {
+            session_start();
+            session([
+                'user' => $user->user_id,
+                'role' => $user->user_role,
+                'user-name' => $user->user_name,
+                'user-image' => $user->user_image
+            ]);
+            return redirect('/');
+        }
+    }
+}
