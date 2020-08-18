e8c528f5c978694cced071b22b227fd808a41b4f
Le Van Trong Nghia <39544463+Alcaztra@users.noreply.github.com>
2020-08-18 15:55:23 +0700

    create middleware

---
+++ app/Http/Middleware/IsTeacher.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsTeacher
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->session()->get('role') == 2) {
+            return $next($request);
+        }
+        return redirect('/');
+    }
+}
